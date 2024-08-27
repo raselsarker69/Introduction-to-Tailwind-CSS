50114209e6ebdff102eae1ed99576eb0ff5b18a2
Your Name <your.email@example.com>
2024-08-27 21:06:36 +0600

    tailwind css practice code

---
+++ Practice-Tailwind-CSS/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex">
+        <div className='flex-1 border border-green-700'>Bangladesh</div>
+        <div className='flex-2 border border-red-600'>Afganisthan</div>
+      </div>
+    </>
+  )
+}
+
+export default App
